eb0101bf684954e2e6fd28cadb2bc777c1ea5a14
Théo Pinchon <theo.pinchon9@etu.univ-lorraine.fr>
2022-06-08 12:06:54 +0200

    constructeur avec un fichier texte

--- src/GraphListe.java
+++ src/GraphListe.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,12 +17,73 @@ public class GraphListe implements Graphe
      */
     private List<Noeud> ensNoeuds;
 
-
+    /**
+     * constructeur vide de graphe
+     */
     public GraphListe() {
         this.ensNom = new ArrayList<String>();
         this.ensNoeuds = new ArrayList<Noeud>();
     }
 
+    /**
+     * constructeur qui creer un graphe selon un fichier texte donne
+     * @param nomFichier nom du fichier a charger
+     */
+    public GraphListe(String nomFichier) throws IOException {
+        //creation basique
+        this.ensNom = new ArrayList<String>();
+        this.ensNoeuds = new ArrayList<Noeud>();
+
+        // ouvrir fichier
+        FileReader fichier = new FileReader(nomFichier);
+        BufferedReader bfRead = new BufferedReader(fichier);
+
+        // lecture des cases
+        String ligne = bfRead.readLine();
+
+        while(ligne!=null){
+            String dpt="faux";
+            String dest="faux";
+            int cout=0;
+            boolean Bdepart = false;
+            boolean Bdestination = false;
+            boolean Bcout = false;
+            String res = "";
+            for (int i = 0; i < ligne.length(); i++) {
+                if (ligne.charAt(i)=='\t' || i==ligne.length()-1){
+                    if (!Bdepart){
+                        dpt=res;
+                        res="";
+                        Bdepart=true;
+                    }else if (!Bdestination){
+                        dest=res;
+                        res="";
+                        Bdestination=true;
+                    }else if (!Bcout){
+                        res+=ligne.charAt(i);
+                        cout=Integer.parseInt(res);
+                        res="";
+                        Bcout=true;
+                    }
+                }else {
+                    res += ligne.charAt(i);
+                }
+            }
+            ajouterArc(dpt,dest,cout);
+
+            // lecture
+            ligne = bfRead.readLine();
+        }
+        // ferme fichier
+        bfRead.close();
+    }
+
+    /**
+     * methode qui creer un arc entre deux noeuds
+     * @param depart noeud de depart
+     * @param destination noeud de destination
+     * @param cout cout de l arc
+     */
     public void ajouterArc(String depart, String destination, double cout)
     {
        if(!this.ensNom.contains(depart))
@@ -76,7 +141,7 @@ public class GraphListe implements Graphe
      * @return string
      */
     public String toString(){
-        List<Noeud> actuel = this.ensNoeuds;
+        List<Noeud> actuel = new ArrayList<>(this.ensNoeuds);
         String res = "";
         while(!actuel.isEmpty()){
             int i=0;
@@ -87,6 +152,26 @@ public class GraphListe implements Graphe
             res+=actuel.get(i).toString();
             actuel.remove(i);
         }
+        return res;
+    }
+
+    public String toGraphviz(String nomGraph){
+        List<Noeud> actuel = new ArrayList<>(this.ensNoeuds);
+        String res = "";
+        res += "digraph " + nomGraph + " {\n";
+        while(!actuel.isEmpty()){
+            int i=0;
+            for (int j = 0; j < actuel.size(); j++) {
+                if (actuel.get(i).getNom().compareTo(actuel.get(j).getNom())>0)
+                    i=j;
+            }
+            for (int j = 0; j < actuel.get(i).getAdj().size(); j++) {
+                Arc arc = actuel.get(i).getAdj().get(j);
+                res += actuel.get(i).getNom() + " -> " + arc.getDest() + " [label = " +arc.getCout()+"]\n";
+            }
+            actuel.remove(i);
+        }
+        res += "}";
         return res;
     }
 
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,11 @@
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
-        GraphListe graphe = new GraphListe();
-        graphe.ajouterArc("A","B",12);
-        graphe.ajouterArc("B","E",11);
-        graphe.ajouterArc("E","D",43);
-        graphe.ajouterArc("A","D",87);
-        graphe.ajouterArc("C","A",19);
-        graphe.ajouterArc("D","B",23);
-        graphe.ajouterArc("D","C",10);
+    public static void main(String[] args) throws IOException {
+        GraphListe graphe = new GraphListe("Graphes/Graphe1.txt");
         System.out.println(graphe.toString());
+        System.out.println(graphe.toGraphviz("G"));
     }
 
 }
