c8334877953ba50a81c8813c30bcf4ddc27d9337
Théo Pinchon <theo.pinchon9@etu.univ-lorraine.fr>
2022-06-13 17:37:01 +0200

    ajout du temps

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,34 @@
+import java.io.File;
 import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        GraphListe graphe = new GraphListe("Graphes/GrapheBoucle");
         BellmanFord bm = new BellmanFord();
         Dijkstra dj = new Dijkstra();
+        File f = new File("Graphes");
+        File[] graphes = f.listFiles();
 
-        System.out.println("Avec la méthode de BellmanFord :");
-        Valeur v = bm.resoudre(graphe,"A");
-        System.out.println(v);
+        for (File current: graphes) {
+            GraphListe graphe = new GraphListe(current.getPath());
 
+            //Bellman Ford
+            Instant debutBellman = Instant.now();
+            Valeur v = bm.resoudre(graphe,"1");
+            Instant finBellman = Instant.now();
+            Duration temps = Duration.between(debutBellman,finBellman);
 
-        System.out.println("Avec la méthode de Dijkstra :");
-        Valeur v2 = dj.resoudre(graphe,"A");
-        System.out.println(v2);
+            //Dijkstra
+            Instant debutDijkstra = Instant.now();
+            Valeur v2 = dj.resoudre(graphe,"1");
+            Instant finDijkstra = Instant.now();
+            Duration temps2 = Duration.between(debutDijkstra,finDijkstra);
 
+            //affichage
+            System.out.println(current.getName()+" : "+temps.toNanos()+"\t"+temps2.toNanos());
+        }
     }
 }
