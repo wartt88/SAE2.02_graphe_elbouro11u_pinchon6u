42518d3da66f29916ac9d04c456a739055de7486
ElBouroumi Anas <gon.fxx@gmail.com>
2022-06-13 16:58:58 +0200

    modification du main et ajout du GrapheBoucle

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,18 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        GraphListe graphe = new GraphListe("Graphes/Graphe000000.txt");
+        GraphListe graphe = new GraphListe("Graphes/GrapheBoucle");
         BellmanFord bm = new BellmanFord();
         Dijkstra dj = new Dijkstra();
 
+        System.out.println("Avec la méthode de BellmanFord :");
         Valeur v = bm.resoudre(graphe,"A");
-        Valeur v2 = dj.resoudre(graphe,"A");
         System.out.println(v);
+
+
+        System.out.println("Avec la méthode de Dijkstra :");
+        Valeur v2 = dj.resoudre(graphe,"A");
         System.out.println(v2);
-        /**
-        List<String> retour =v.calculerChemin("C");
-        for (String r: retour) {
-            System.out.println(r);
-        }
-         */
 
     }
 }
