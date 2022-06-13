4470f2f4f331aae21d9536ba8b20eeced14b2c39
Théo Pinchon <theo.pinchon9@etu.univ-lorraine.fr>
2022-06-13 14:58:20 +0200

    dijkstra

---
+++ src/Dijkstra.java
@@ -0,0 +1,71 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dijkstra {
+    /**
+     * Entrees :
+     * G un graphe orient ́e avec une pond ́eration (poids) positive des arcs
+     * A un sommet (d ́epart) de G
+     * <p>
+     * Debut
+     * Q <- {} // utilisation d’une liste de noeuds `a traiter
+     * Pour chaque sommet v de G faire
+     * v.distance <- Infini
+     * v.parent <- Ind efini
+     * Q <- Q U {v} // ajouter le sommet v a la liste Q
+     * Fin Pour
+     * <p>
+     * A.distance <- 0
+     * Tant que Q est un ensemble non vide faire
+     * u <- un sommet de Q telle que u.distance est minimale
+     * Q <- Q \ {u} // enlever le sommet u de la liste Q
+     * Pour chaque sommet v de Q tel que l’arc (u,v) existe faire
+     * D <- u.distance + poids(u,v)
+     * Si D < v.distance
+     * Alors v.distance <- D
+     * v.parent <- u
+     * Fin Si
+     * Fin Pour
+     * Fin Tant que
+     * Fin
+     */
+
+    public Valeur resoudre(Graphe g, String depart) {
+        Valeur valeur = new Valeur();
+        List<String> aTraiter = new ArrayList<>();
+
+        //initialisation de l'algo :
+        for (int i = 0; i < g.listeNoeuds().size(); i++) {
+            String nom = g.listeNoeuds().get(i);
+            valeur.setValeur(nom, Double.MAX_VALUE);
+            valeur.setParent(nom, null);
+            aTraiter.add(nom);
+        }
+
+        //on met la première valeur à 0 :
+        valeur.setValeur(depart, 0);
+
+        // début de la méthode point fixe :
+        while (!aTraiter.isEmpty()) {
+            //recherche du chemin minimal
+            String noeud = aTraiter.get(0);
+            for (String current : aTraiter) {
+                if (valeur.getValeur(current) < valeur.getValeur(noeud)) {
+                    noeud = current;
+                }
+            }
+            aTraiter.remove(noeud);
+
+            List<Arc> list = g.suivants(noeud);
+            for (int j = 0; j < list.size(); j++) {
+                double somme = valeur.getValeur(noeud) + list.get(j).getCout();
+                if (valeur.getValeur(list.get(j).getDest()) > somme) {
+                    valeur.setValeur(list.get(j).getDest(), somme);
+                    valeur.setParent(list.get(j).getDest(), noeud);
+                }
+            }
+        }
+        return valeur;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,18 @@ public class Main {
     public static void main(String[] args) throws IOException {
         GraphListe graphe = new GraphListe("Graphes/Graphe000000.txt");
         BellmanFord bm = new BellmanFord();
+        Dijkstra dj = new Dijkstra();
 
         Valeur v = bm.resoudre(graphe,"A");
-
+        Valeur v2 = dj.resoudre(graphe,"A");
+        System.out.println(v);
+        System.out.println(v2);
+        /**
         List<String> retour =v.calculerChemin("C");
         for (String r: retour) {
             System.out.println(r);
         }
+         */
 
     }
 }
