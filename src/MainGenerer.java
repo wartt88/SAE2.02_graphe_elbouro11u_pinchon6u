893054a883a4898d5ecedcf142fcc8e0b6ce6234
Théo Pinchon <theo.pinchon9@etu.univ-lorraine.fr>
2022-06-14 10:43:08 +0200

    fichier excel qui contient les calculs de temps

--- src/MainGenerer.java
+++ src/MainGenerer.java
@@ -1,6 +1,13 @@
 public class MainGenerer {
+
+
+
+    //execution avec un parametre qui est le nombre de noeud que doit comporter le graphe
     public static void main(String[] args) {
-        GraphListe graphe = new GraphListe(50);
+        int nb=10;
+        if (args.length>0)
+            nb = Integer.parseInt(args[0]);
+        GraphListe graphe = new GraphListe(nb);
         System.out.println(graphe.toGraphviz("invention"));
     }
 }
