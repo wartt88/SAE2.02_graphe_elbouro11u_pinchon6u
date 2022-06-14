de7ca95d527c259ff7691e696cf236fb3f87dacd
Théo Pinchon <theo.pinchon9@etu.univ-lorraine.fr>
2022-06-14 14:03:19 +0200

    ajout de la classe GrapheLabyrinthe

--- src/mains/MainLabyGraphe.java
+++ src/mains/MainLabyGraphe.java
@@ -2,6 +2,7 @@ package mains;
 
 import algorithme.Dijkstra;
 import graphes.*;
+import laby.GrapheLabyrinthe;
 import laby.Labyrinthe;
 
 import java.io.IOException;
@@ -13,12 +14,19 @@ public class MainLabyGraphe {
 
     public static void main(String[] args) throws IOException {
         Labyrinthe lb = new Labyrinthe("labySimple/laby1.txt");
-        GraphListe g = lb.genererGraphe();
+        Dijkstra dj = new Dijkstra();
         //System.out.println(g.toGraphviz("labyrinthe"));
 
-        Dijkstra dj = new Dijkstra();
+        // affichage du chemin en utilisant la methode genererGraphe de la class Labyrinthe
+        GraphListe g = lb.genererGraphe();
         Valeur v = dj.resoudre(g,"\"(1,1)\"");
         //calcul du chemin le plus court pour atteindre le noeud "(5,5)"
         System.out.println(v.calculerChemin("\"(5,5)\""));
+
+        // affichage du chemin en utilisant la class d adaptateur GrapheLabyrinthe
+        GrapheLabyrinthe gl = new GrapheLabyrinthe(lb);
+        Valeur v2 = dj.resoudre(gl,"\"(1,1)\"");
+        //calcul du chemin le plus court pour atteindre le noeud "(5,5)"
+        System.out.println(v2.calculerChemin("\"(5,5)\""));
     }
 }
